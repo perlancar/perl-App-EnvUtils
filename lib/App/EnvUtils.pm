9fabe2886bcbb9888183a52ddedd32ff9b8f7566
perlancar (on notebook-dell-xps13-9315-jun2024) <perlancar@gmail.com>
2024-11-12 15:10:07 +0700

    Initial commit

---
+++ lib/App/EnvUtils.pm
@@ -0,0 +1,24 @@
+package App::EnvUtils;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+#ABSTRACT: Run commands with some environment variables set
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<IPC::System::Options>
+
+=cut
